0cbca1c662f9fdf626c69d354ac69ef3081b4208
joel <joel.as.0808@gmail.com>
2020-06-16 14:10:58 +0200

    run.sh arbitary paths

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ module load slurm-drmaa/1.0.7
 output_dir=$1
 wkdir=$(pwd)
 
-resources="/data/ref_data/gatk/GRCh37.75/"
-input_location="/projects/wp2/nobackup/CGU_2019_5_Twist/200320_NDX550407_RUO_0007_AH5YFMAFX2"
-data_location="/projects/wp4/nobackup/workspace/joel_test/pgx_module"
-ref_location="/data/ref_genomes/bcbio-nextgen/gatk"
+#Note: set these paths to your location
+resources="/path/to/dbsnp/dir"
+input_location="/path/to/sample/bams/"
+data_location="/path/to/pgx_module"
+ref_location="/path/to/ref_dir"
 
 cp data/example_config.yaml $output_dir/config.yaml
 echo "run_location: $wkdir" >> $output_dir/config.yaml
@@ -21,4 +22,4 @@ snakemake -p --drmaa "-A wp4 -s -p core -t {cluster.time} -n {cluster.n} " \
 	"--bind $resources --bind $input_location --bind $data_location --bind $ref_location" \
 	--cluster-config ${wkdir}/cluster.json \
 	--rerun-incomplete \
-	--jobs 1
+	--jobs 16
